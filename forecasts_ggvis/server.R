63996132fb4e28e232acafc5bd6611e8e2325d86
ellisp <peter.ellis2013nz@gmail.com>
2015-01-15 17:50:10 +1300

    Add tooltips

--- forecasts_ggvis/server.R
+++ forecasts_ggvis/server.R
@@ -21,29 +21,49 @@ shinyServer(function(input, output) {
    my_forecasts %>%
       ggvis(x = ~Year, y = ~TotalVisitorArrivals, stroke = ~Type) %>%
       layer_lines() %>%
+      layer_points(fill = ~Type) %>%
       add_axis("x", title="", format="04d") %>%
       add_axis("y", title="Total visitor arrivals", format="d", title_offset = 65) %>%
+      add_tooltip(function(input){
+         paste(format(round(input$TotalVisitorArrivals, -3), big.mark=","),
+               "<br>arrivals</br>")
+         }, on="click") %>%
       bind_shiny("TotalVisitorArrivals") 
    
    my_forecasts %>%
       ggvis(x = ~Year, y = ~SpendPerDay, stroke = ~Type) %>%
       layer_lines() %>%
+      layer_points(fill = ~Type) %>%
       add_axis("x", title="", format="04d") %>%
       add_axis("y", title="Spend per day", format="$d", title_offset = 65) %>%
+      add_tooltip(function(input){
+         paste("$", format(round(input$SpendPerDay, -2), big.mark=","),
+               "<br>per day</br>")
+      }, on="click") %>%
       bind_shiny("SpendPerDay") 
    
    my_forecasts %>%
       ggvis(x = ~Year, y = ~AvLengthOfStay, stroke = ~Type) %>%
       layer_lines() %>%
+      layer_points(fill = ~Type) %>%
       add_axis("x", title="", format="04d") %>%
       add_axis("y", title="Average length of stay", format="d", title_offset = 65) %>%
+      add_tooltip(function(input){
+         paste(format(round(input$AvLengthOfStay, 1), big.mark=","),
+               "<br>days</br>")
+      }, on="click") %>%
       bind_shiny("AvLengthOfStay") 
    
    my_forecasts %>%
       ggvis(x = ~Year, y = ~TotalVisitorSpend, stroke = ~Type) %>%
       layer_lines() %>%
+      layer_points(fill = ~Type) %>%
       add_axis("x", title="", format="04d") %>%
-      add_axis("y", title="Total spend", format="$d", title_offset = 65) %>%
+      add_axis("y", title="Total spend ($m)", format="$d", title_offset = 65) %>%
+      add_tooltip(function(input){
+         paste("$", format(round(input$TotalVisitorSpend, 1), big.mark=","),
+               "<br>million</br>")
+      }, on="click") %>%
       bind_shiny("TotalVisitorSpend") 
    
    
