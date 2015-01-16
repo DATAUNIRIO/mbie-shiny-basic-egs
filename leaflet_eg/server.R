ab24fad01b1c210f615bd8d6a01c8d893768f2be
ellisp <peter.ellis2013nz@gmail.com>
2015-01-17 09:22:40 +1300

    working version of leaflet map with interactivity

--- leaflet_eg/server.R
+++ leaflet_eg/server.R
@@ -10,19 +10,23 @@ m <- leaflet()  %>% addTiles() %>%  setView(174.7772, -41.2889, zoom = 6)
 load("visits_by_yr_by_country.rda")
 
 shinyServer(function(input, output) {
-   the_data <- visited_m_all %>%
-      filter(Year == 2013 & Country == "Australia")
+   the_data <- reactive({
+      visited_m_all %>%
+         filter(Year == input$year & Country == input$country)
+   })
    
    
    # Print that map, centering it in Wellington and adding circles proportionate to number of nights and with 
-   map <- m %>%
-            addCircleMarkers( the_data$Longitude, 
-                              the_data$Latitude, 
-                              radius = sqrt(the_data$Nights / max(the_data$Nights)) * 50,
-                              popup = the_data$PopUp)
+   map <- reactive({
+      m %>%
+            addCircleMarkers( the_data()$Longitude, 
+                              the_data()$Latitude, 
+                              radius = sqrt(the_data()$Nights / max(the_data()$Nights)) * 50,
+                              popup = the_data()$PopUp)
+   })
    
    
-   output$myMap <- renderLeaflet(map)
+   output$myMap <- renderLeaflet(map())
    
 
 })
--- leaflet_eg/ui.R
+++ leaflet_eg/ui.R
@@ -1,11 +1,29 @@
 library(shiny)
 library(ggvis)
 library(leaflet)
+
+load("countries_and_years.rda")
+
 shinyUI(fixedPage(
    
-   h1("Visits in 2013"),
+   h1("Visits"),
+   fixedRow(
+      column(width=4, 
+             selectInput("country", "Country of origin", choices = countries)
+             ),
+      column(width=4, 
+             p("")
+             #  submitButton("Get new map")
+      ),
+      column(width=4, 
+             selectInput("year", "Year of visit", choices = years)
+      )
+      
+      ),
                 
-   leafletOutput('myMap', height=800)
+   fixedRow(
+      leafletOutput('myMap', height=700)
+   )
       
       
    )
