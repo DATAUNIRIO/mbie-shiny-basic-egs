55297b1444c8acebfa44a832be1a0754b1d989e1
ellisp <peter.ellis2013nz@gmail.com>
2015-01-19 12:40:32 +1300

    Add CSS to forecasts ggvis example

--- forecasts_ggvis/ui.R
+++ forecasts_ggvis/ui.R
@@ -5,6 +5,37 @@ load("forecasts.rda")
 
 # Define UI for application that draws a histogram
 shinyUI(fixedPage(
+   # insert some CSS.  Down the track this would be stored in a separate file.
+   # Colours used on MBIE pages seem to be 76797c (dark blue) 436976 (paler blue) 8cacbb (paler again)
+   # dee7ec (very pale blue) CC9933 (sort of brown)
+   tags$head(
+      tags$style(HTML("
+                      @import url('//fonts.googleapis.com/css?family=Nunito|Cabin:400,700');
+                      
+                      body {
+                      font-family: 'Nunito';
+                      color: #436976;
+                      }
+                      
+                      h2 {
+                      font-weight: 700;
+                      line-height: 1.1;
+                      color: #8cacbb;
+                      }
+                      
+                      pop {
+                      font-family: 'Nunito';
+                      color: #CC9933;
+                      }
+                      
+                      
+                      .selectize-dropdown, .selectize-input { 
+                      font-family: 'Nunito';
+                      color:#436976;
+                      }
+                      
+                      "))
+      ),
    
    # Application title
    titlePanel("New Zealand tourism forecasts"),
@@ -22,10 +53,8 @@ shinyUI(fixedPage(
       column(width = 4,  ggvisOutput("AvLengthOfStay")),
       column(width=4),
       column(width = 4,  ggvisOutput("TotalVisitorSpend"))
-      
-
-
-      
    )
+      
+
    
 ))
