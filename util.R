c85bb3420930c0bd5937943a36a759b0547d7c25
ellisp <peter.ellis2013nz@gmail.com>
2015-01-21 08:15:08 +1300

    delete util.R

--- util.R
+++
@@ -1,9 +0,0 @@
-
-library(shinyapps)
-
-setAccountInfo(name='peteratmbie', 
-                          token='3BE0B1C04F9D288E16F631B7F018925D', 
-                          secret='oMkmQlOIgnSlML8S7ZZblp8ntFgoxj9WZACa4Bbo')
-
-deployApp("forecasts_ggvis")
-deployApp("simple_map")
