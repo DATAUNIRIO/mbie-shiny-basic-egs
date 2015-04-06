2e18b737fd4128e3684955624e7cfa422e7db527
ellisp <peter.ellis2013nz@gmail.com>
2015-04-06 16:21:19 +1200

    add more comments on what the training is about

--- session3/integrate.R
+++ session3/integrate.R
@@ -2,4 +2,21 @@ library(shinyapps)
 source("prep.R")
 
 setwd("session3")
-deployApp("v3", appName = "ivs_demo", account = "mbienz")
+
+
+# deployApp("v3", appName = "ivs_demo", account = "mbienz")
+
+
+# This training folder includes 3 versions of the same IVS shiny app.  
+# v1 contains just the preparation script (which downloads and reshpaes data) and the minimum interactivity
+#    and plots
+# v2 demonstrates customisation of the plots
+# v3 adds interactivity in three different ways - mouse hover, and mouseclick via ggvis; and javascript in
+#    combination with moving data between server.r and ui.r that wasn't created with an R function
+
+# By the end of this session you should have familiarity and examples of:
+#  * ggvis plot polishing inlcuding formats of axis labels; legends; controlling width and height; and a few
+#    other things
+#  * add a data-driven tooltip to a ggvis plot
+#  * have an event-driven update of some of the input list by clicking on a ggvis plot
+#  * at least be able to get started for incorporating javascript into shiny apps.
